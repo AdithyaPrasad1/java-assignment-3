0c9a59fb7ee54703edb6627f0722f180a24a4d8a
ADITHYA PRASAD <134258597+AdithyaPrasad1@users.noreply.github.com>
2023-06-20 17:46:24 +0530

    Add files via upload

---
+++ oddNumsum.js
@@ -0,0 +1,10 @@
+var readlinesync=require('readline-sync');
+let limit=readlinesync.question('enter the limit : ');
+limit=parseInt(limit);
+sum=0
+for (var i=1;i<=limit;i++){
+    if(i%2==1){
+        sum=sum+i;
+        console.log(i)
+    }
+}
